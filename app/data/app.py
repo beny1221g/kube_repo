e4c9e61e03e2cc8141124ff2f96dd7c2fc4879a8
BZ\beny1 <beny1221g@gmail.com>
2024-08-31 12:00:03 +0300

    add /health and /readiness

--- app/data/app.py
+++ app/data/app.py
@@ -1,5 +1,13 @@
 from flask import Flask
+
+
 app = Flask(__name__)
+@app.route('/health')
+def health_check():
+    return 'Health',200
+@app.route('/readiness')
+def health_check():
+    return 'Ready',200
 
 @app.route('/')
 def hello_world():
