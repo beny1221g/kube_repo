3e614d3cceb9a066a42d9d324f64df670ed53460
BZ\beny1 <beny1221g@gmail.com>
2024-08-31 21:33:57 +0300

    add /health and /readiness

--- app/data/app.py
+++ app/data/app.py
@@ -1,17 +1,18 @@
 from flask import Flask
 
-
 app = Flask(__name__)
+
 @app.route('/health')
 def health_check():
-    return 'Health',200
+    return 'Health', 200
+
 @app.route('/readiness')
-def health_check():
-    return 'Ready',200
+def readiness_check():  # Renamed to `readiness_check` to avoid conflict
+    return 'Ready', 200
 
 @app.route('/')
 def hello_world():
-    return 'Hso, World!'
+    return 'Hello, World!', 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
